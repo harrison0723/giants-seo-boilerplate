585ba526001fe8f031829f52bf4dfaf7aa170088
Harrison Lo <harrison0723@gmail.com>
2019-09-27 13:37:28 +0800

    Remove: unnecessary firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -12,8 +12,5 @@ const firebaseConfig = {
 }
 
 firebase.initializeApp(firebaseConfig)
-const firestore = firebase.firestore()
-const settings = { timestampsInSnapshots: true }
-firestore.settings(settings)
 
 export default firebase
